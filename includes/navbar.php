47b01984d21244d0cff0d64cca541082f55aa670
Insiderfyr <burcadenis21@gmail.com>
2025-11-09 13:38:23 +0200

    A

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Acas</a>
+                    <a class="nav-link" href="index.php">Acasa</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="events/list_events.php">Evenimente</a>
@@ -19,7 +19,7 @@
                     <a class="nav-link" href="login.php">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link btn btn-primary text-white ms-2 px-3" href="register.php">Înregistrare</a>
+                    <a class="nav-link btn btn-primary text-white ms-2 px-3" href="register.php">Inregistrare</a>
                 </li>
             </ul>
         </div>
